364f3a231e109dd21e57da35d7533166baa03045
魏武涛 <mac@jingtaoMac.local>
2017-09-02 22:01:46 +0800

    fix: 文件组织方式更改，放弃framework方式

---
+++ Source/RongCloudIMDataRequest.m
@@ -0,0 +1,15 @@
+//
+//  RongCloudIMDataRequest.m
+//  RongCloudIMManager
+//
+//  Created by Mac on 2017/8/31.
+//  Copyright © 2017年 LiYou. All rights reserved.
+//
+
+#import "RongCloudIMDataRequest.h"
+#import <RongIMKit/RongIMKit.h>
+#import <RongIMLib/RongIMLib.h>
+
+@implementation RongCloudIMDataRequest
+
+@end
